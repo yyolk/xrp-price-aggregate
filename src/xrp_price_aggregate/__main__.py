ba683ebe42fe3a0d8e311c4a44905c3e5a893491
Joseph Chiocchi <joe@yolk.cc>
2021-08-07 15:46:35 -0500

    add simple flags for invoking module that all could mean `not fast`

--- src/xrp_price_aggregate/__main__.py
+++ src/xrp_price_aggregate/__main__.py
@@ -1,4 +1,9 @@
+import sys
+
 from . import as_json
 
 
-print(as_json(count=2, delay=0.25, fast=True))
+# call with the fast parameter by default, unless provided with -exhaustive
+fast = not sys.argv[-1].endswith(("--exhaustive", "-E", "-L", "--long", "--ccxt"))
+
+print(as_json(count=2, delay=0.25, fast=fast))

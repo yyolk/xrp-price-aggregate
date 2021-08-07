95dacf883779242a9efbd38aab875e306097b283
Joseph Chiocchi <joe@yolk.cc>
2021-08-07 15:47:26 -0500

    update comment

--- src/xrp_price_aggregate/__main__.py
+++ src/xrp_price_aggregate/__main__.py
@@ -3,7 +3,8 @@ import sys
 from . import as_json
 
 
-# call with the fast parameter by default, unless provided with -exhaustive
+# call with the fast parameter by default...
+# ...unless provided with a short or long option
 fast = not sys.argv[-1].endswith(("--exhaustive", "-E", "-L", "--long", "--ccxt"))
 
 print(as_json(count=2, delay=0.25, fast=fast))
